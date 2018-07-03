64a99fdc47f4aefb40feaadb7f8a2986509b163a
amrit-github <amritsuman1996@gmail.com>
2018-07-03 15:40:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amrit/hellokitty/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.amrit.hellokitty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
