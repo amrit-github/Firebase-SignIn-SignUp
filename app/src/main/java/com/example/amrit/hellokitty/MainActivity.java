68577424823f5cffe9594f7ecbb17f4f725146b3
amrit-github <amritsuman1996@gmail.com>
2018-07-03 15:42:48 +0530

    2nd commit 3rd commit

--- app/src/main/java/com/example/amrit/hellokitty/MainActivity.java
+++ app/src/main/java/com/example/amrit/hellokitty/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     private Button button;
+    int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
